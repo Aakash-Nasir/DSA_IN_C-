25e4f933726a00ead82a4bac9030ac5e22c01f07
Aakash <147227192+Aakash-Nasir@users.noreply.github.com>
2024-08-11 23:09:43 +0530

    Added Code

---
+++ IF-ELSE/+veOr-ve.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // check number is positive or negative
+    cout<<"enter a number \n";
+    int n;
+    cin>>n;
+    if(n>=0)
+    {
+        cout<<"number is positive";
+    }
+    else
+    {
+        cout<<"number is negative";
+    }
+    return 0;
+}
---
+++ IF-ELSE/EvenOrOdd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+// check number is even or odd
+{
+  cout<<"enter number \n";
+  int n;
+  cin>>n;
+  if(n%2==0)
+  {
+  cout<<"number is even";
+  }
+else
+   {
+     cout<<"number is odd";
+    }
+    return 0;
+   }
