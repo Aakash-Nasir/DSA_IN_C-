f973ad6793807a44db38ee00cded00bf56765fdb
Aakash <147227192+Aakash-Nasir@users.noreply.github.com>
2024-08-27 06:59:19 +0530

    Search in 2D Array

---
+++ ARRAYS/TwoDimensionalArray/SearchInSorted2DMatrix.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool SearchInASorted2DMatrix(vector<vector<int>>& matrix, int target){
+
+if(matrix.empty()) {
+return false;
+}
+int m =matrix.size();
+int n = matrix[0].size();
+int i=0 , j = n-1;
+
+while (i<m && j>=0)
+{
+    
+}
+
+}
